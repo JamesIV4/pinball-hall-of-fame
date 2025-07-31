925527d3e86ee802f9dcbc976aef62995b5f9377
James Pound <james.pound@genesys.com>
2025-07-31 16:12:47 -0400

    Added docs directory

---
+++ docs/_next/static/m1-x-G2Jmz6_HIVp6Pt-y/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/7112840a-a7b5aeaaf466da50.js","static/chunks/233-46aab7bc4bb3a8ac.js","static/chunks/pages/index-40c55e3573d7d333.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
