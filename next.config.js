bfcff089e0410125514574afb31c0615712b7ef4
James Pound <james.pound@genesys.com>
2025-08-01 09:39:18 -0400

    Updated asset paths

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+const repo = 'pinball-hall-of-fame';
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: "export", // enables `next export`
   trailingSlash: true, // better for GitHub Pages
-  basePath: "/pinball-hall-of-fame", // GitHub Pages subdirectory
+  basePath: process.env.NODE_ENV === 'production' ? `/${repo}` : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? `/${repo}/` : '',
   distDir: "docs", // output to docs folder for GitHub Pages
   images: {
     unoptimized: true, // since export produces static HTML only
