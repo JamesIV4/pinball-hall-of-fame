1e9ec69a8903c1998aaf53c9b23bcb1786d6e6db
James Pound <james.pound@genesys.com>
2025-08-01 09:32:05 -0400

    Fixed base path for github pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   output: "export", // enables `next export`
   trailingSlash: true, // better for GitHub Pages
+  basePath: "/pinball-hall-of-fame", // GitHub Pages subdirectory
   distDir: "docs", // output to docs folder for GitHub Pages
   images: {
     unoptimized: true, // since export produces static HTML only
