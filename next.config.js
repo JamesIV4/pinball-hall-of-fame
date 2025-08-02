51f6d5baaa591cf432161662150d451d88e968ed
James Pound <james.pound@genesys.com>
2025-08-02 11:42:41 -0400

    Changed output directory to docs again

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ module.exports = {
   trailingSlash: true, // better for GitHub Pages
   basePath: process.env.NODE_ENV === "production" ? `/${repo}` : "",
   assetPrefix: process.env.NODE_ENV === "production" ? `/${repo}/` : "",
+  distDir: "docs", // output to docs folder for GitHub Pages
   images: {
     unoptimized: true, // since export produces static HTML only
   },
