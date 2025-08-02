3ca9e7ade964d402453a325306ec2e204c540e57
James Pound <james.pound@genesys.com>
2025-08-02 11:36:32 -0400

    Removed static site from the repo since it gets built by github pages

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const repo = "pinball-hall-of-fame";
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  output: "export", // enables `next export`
   trailingSlash: true, // better for GitHub Pages
   basePath: process.env.NODE_ENV === "production" ? `/${repo}` : "",
   assetPrefix: process.env.NODE_ENV === "production" ? `/${repo}/` : "",
